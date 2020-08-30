edfb2cbb42269eb906a9d6db3e8eae84ed919d82
White <white.shan@badanamu.com.cn>
2020-08-30 13:27:59 +0800

    update route

--- route/orders.go
+++ route/orders.go
@@ -3,6 +3,7 @@ package route
 import (
 	"net/http"
 	"strconv"
+	"strings"
 	"xg/entity"
 	"xg/service"
 
@@ -29,12 +30,7 @@ func (s *Server) createOrder(c *gin.Context) {
 }
 
 func (s *Server) searchOrder(c *gin.Context) {
-	req := new(entity.SearchOrderCondition)
-	err := c.ShouldBind(req)
-	if err != nil {
-		s.responseErr(c, http.StatusBadRequest, err)
-		return
-	}
+	req := buildOrderCondition(c)
 	user, ok := s.getJWTUser(c)
 	if !ok {
 		return
@@ -49,12 +45,7 @@ func (s *Server) searchOrder(c *gin.Context) {
 }
 
 func (s *Server) searchOrderWithAuthor(c *gin.Context) {
-	req := new(entity.SearchOrderCondition)
-	err := c.ShouldBind(req)
-	if err != nil {
-		s.responseErr(c, http.StatusBadRequest, err)
-		return
-	}
+	req := buildOrderCondition(c)
 	user, ok := s.getJWTUser(c)
 	if !ok {
 		return
@@ -69,12 +60,7 @@ func (s *Server) searchOrderWithAuthor(c *gin.Context) {
 }
 
 func (s *Server) searchOrderWithOrgID(c *gin.Context) {
-	req := new(entity.SearchOrderCondition)
-	err := c.ShouldBind(req)
-	if err != nil {
-		s.responseErr(c, http.StatusBadRequest, err)
-		return
-	}
+	req := buildOrderCondition(c)
 	user, ok := s.getJWTUser(c)
 	if !ok {
 		return
@@ -109,12 +95,7 @@ func (s *Server) getOrderByID(c *gin.Context) {
 }
 
 func (s *Server) searchPendingPayRecord(c *gin.Context) {
-	req := new(entity.SearchPayRecordCondition)
-	err := c.ShouldBind(req)
-	if err != nil {
-		s.responseErr(c, http.StatusBadRequest, err)
-		return
-	}
+	req := buildSearchPayRecordCondition(c)
 	user, ok := s.getJWTUser(c)
 	if !ok {
 		return
@@ -127,4 +108,74 @@ func (s *Server) searchPendingPayRecord(c *gin.Context) {
 		return
 	}
 	s.responseSuccessWithData(c, "data", records)
+}
+
+func buildSearchPayRecordCondition(c *gin.Context) *entity.SearchPayRecordCondition {
+	payRecordIds := c.Query("pay_record_ids")
+	orderIds := c.Query("order_ids")
+	authorIds := c.Query("author_ids")
+	mode := c.Query("mode")
+	status := c.Query("status")
+	orderBy := c.Query("order_by")
+	page := c.Query("page")
+	pageSize := c.Query("page_size")
+
+	return &entity.SearchPayRecordCondition{
+		PayRecordIDList: parseInts(payRecordIds),
+		OrderIDList:     parseInts(orderIds),
+		AuthorIDList:    parseInts(authorIds),
+		Mode:            parseInt(mode),
+		StatusList:      parseInts(status),
+
+		OrderBy: orderBy,
+
+		PageSize: parseInt(pageSize),
+		Page:     parseInt(page),
+	}
+}
+
+func buildOrderCondition(c *gin.Context) *entity.SearchOrderCondition {
+	studentIds := c.Query("student_ids")
+	toOrgIds := c.Query("to_org_ids")
+	intentSubjects := c.Query("intent_subjects")
+	publisherID := c.Query("publisher_id")
+	status := c.Query("status")
+	orderBy := c.Query("order_by")
+	page := c.Query("page")
+	pageSize := c.Query("page_size")
+
+	return &entity.SearchOrderCondition{
+		StudentIDList:  parseInts(studentIds),
+		ToOrgIDList:    parseInts(toOrgIds),
+		IntentSubjects: intentSubjects,
+		PublisherID:    parseInt(publisherID),
+
+		Status:  parseInt(status),
+		OrderBy: orderBy,
+
+		PageSize: parseInt(pageSize),
+		Page:     parseInt(page),
+	}
+}
+
+func parseInt(str string) int {
+	id, err := strconv.Atoi(str)
+	if err == nil {
+		return 0
+	}
+	return id
+}
+func parseInts(str string) []int {
+	strList := strings.Split(str, ",")
+	ret := make([]int, 0)
+	for i := range strList {
+		id, err := strconv.Atoi(strList[i])
+		if err == nil {
+			ret = append(ret, id)
+		}
+	}
+	if len(ret) < 1 {
+		return nil
+	}
+	return ret
 }
--- route/route.go
+++ route/route.go
@@ -31,6 +31,21 @@ func Get() *gin.Engine {
 		user.GET("/authority", s.mustLogin, s.listUserAuthority)
 		user.POST("/login", s.login)
 		user.PUT("/password", s.mustLogin, s.updatePassword)
+		user.POST("/", s.mustLogin, s.createUser)
+		user.PUT("/reset", s.mustLogin, s.resetPassword)
+	}
+	users := route.Group("/users")
+	{
+		users.GET("/", s.mustLogin, s.listUsers)
+	}
+
+	role := route.Group("/role")
+	{
+		role.POST("/", s.mustLogin, s.createRole)
+	}
+	roles := route.Group("/roles")
+	{
+		roles.GET("/", s.mustLogin, s.listRoles)
 	}
 
 	student := route.Group("/student")
@@ -46,21 +61,38 @@ func Get() *gin.Engine {
 	subject := route.Group("/subjects")
 	{
 		subject.GET("/:parent_id", s.mustLogin, s.listSubjects)
+		subject.POST("/", s.mustLogin, s.createSubject)
 	}
-
-	org := route.Group("/orgs")
+	org := route.Group("/org")
 	{
-		org.GET("/", s.mustLogin, s.listOrgs)
+		org.POST("/", s.mustLogin, s.createOrg)
+		org.GET("/:id", s.mustLogin, s.getOrgById)
+		org.PUT("/:id/reject", s.mustLogin, s.RejectOrg)
+		org.PUT("/:id/approve", s.mustLogin, s.ApproveOrg)
+	}
+	orgs := route.Group("/orgs")
+	{
+		orgs.GET("/", s.mustLogin, s.listOrgs)
+		orgs.GET("/pending", s.mustLogin, s.listPendingOrgs)
 	}
 
 	order := route.Group("/order")
 	{
 		order.POST("/", s.mustLogin, s.createOrder)
+		order.GET("/:id", s.mustLogin, s.getOrderByID)
+	}
+	orders := route.Group("/orders")
+	{
+		orders.GET("/", s.mustLogin, s.searchOrder)
+		orders.GET("/author", s.mustLogin, s.searchOrderWithAuthor)
+		orders.GET("/org", s.mustLogin, s.searchOrderWithOrgID)
+		orders.GET("/payment", s.mustLogin, s.searchPendingPayRecord)
 	}
 
 	orderSources := route.Group("/order_sources")
 	{
 		orderSources.GET("/", s.mustLogin, s.listOrderSources)
+		orderSources.POST("/", s.mustLogin, s.createOrderSource)
 	}
 
 	return route
