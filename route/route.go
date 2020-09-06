7d5ebe177893257dabb117f9b8daaf775cdf7c4e
White <white.shan@badanamu.com.cn>
2020-09-06 18:29:23 +0800

    update route

--- route/route.go
+++ route/route.go
@@ -16,18 +16,18 @@ func Get() *gin.Engine {
 	s := new(Server)
 
 	route.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"http://localhost"},
 		AllowMethods:     []string{"PUT", "GET", "POST", "DELETE"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "Cookie"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		AllowOriginFunc: func(origin string) bool {
-			return origin == "http://localhost:3000"
+			return origin == "http://localhost"
 		},
 		MaxAge: 12 * time.Hour,
 	}))
-
-	user := route.Group("/user")
+	api := route.Group("/api")
+	user := api.Group("/user")
 	{
 		user.GET("/authority", s.mustLogin, s.listUserAuthority)
 		user.POST("/login", s.login)
@@ -35,45 +35,45 @@ func Get() *gin.Engine {
 		user.POST("/", s.mustLogin, s.hasPermission([]int{entity.AuthManageUser}), s.createUser)
 		user.PUT("/reset/:id", s.mustLogin, s.hasPermission([]int{entity.AuthManageUser}), s.resetPassword)
 	}
-	users := route.Group("/users")
+	users := api.Group("/users")
 	{
 		users.GET("/", s.mustLogin, s.hasPermission([]int{entity.AuthManageUser}), s.listUsers)
 	}
 
-	role := route.Group("/role")
+	role := api.Group("/role")
 	{
 		role.POST("/", s.mustLogin, s.hasPermission([]int{entity.AuthManageRole}), s.createRole)
 	}
-	roles := route.Group("/roles")
+	roles := api.Group("/roles")
 	{
 		roles.GET("/", s.mustLogin, s.listRoles)
 	}
 
-	auth := route.Group("/auths")
+	auth := api.Group("/auths")
 	{
 		auth.GET("/", s.mustLogin, s.listAuth)
 	}
 
-	student := route.Group("/student")
+	student := api.Group("/student")
 	{
 		student.POST("/", s.mustLogin, s.hasPermission([]int{entity.AuthEnterStudent}), s.createStudent)
 		student.GET("/:id", s.mustLogin, s.hasPermission([]int{entity.AuthEnterStudent, entity.AuthListAllOrder, entity.AuthDispatchOrder}), s.getStudentById)
 	}
-	students := route.Group("/students")
+	students := api.Group("/students")
 	{
 		students.GET("/", s.mustLogin, s.hasPermission([]int{entity.AuthListAllOrder, entity.AuthDispatchOrder}), s.searchStudents)
 		students.GET("/private", s.mustLogin, s.hasPermission([]int{entity.AuthEnterStudent}), s.searchPrivateStudents)
 	}
 
-	subject := route.Group("/subject")
+	subject := api.Group("/subject")
 	{
 		subject.POST("/", s.mustLogin, s.hasPermission([]int{entity.AuthManageSubject}), s.createSubject)
 	}
-	subjects := route.Group("/subjects")
+	subjects := api.Group("/subjects")
 	{
 		subjects.GET("/:parent_id", s.mustLogin, s.listSubjects)
 	}
-	org := route.Group("/org")
+	org := api.Group("/org")
 	{
 		org.POST("/", s.mustLogin, s.hasPermission([]int{entity.AuthManageOrg}), s.createOrg)
 		org.GET("/:id", s.mustLogin, s.getOrgById)
@@ -82,48 +82,48 @@ func Get() *gin.Engine {
 		org.PUT("/:id/review/reject", s.mustLogin, s.hasPermission([]int{entity.AuthCheckOrg}), s.RejectOrg)
 		org.PUT("/:id/review/approve", s.mustLogin, s.hasPermission([]int{entity.AuthCheckOrg}), s.ApproveOrg)
 	}
-	orgs := route.Group("/orgs")
+	orgs := api.Group("/orgs")
 	{
 		orgs.GET("/", s.mustLogin, s.listOrgs)
 		orgs.GET("/pending", s.mustLogin, s.hasPermission([]int{entity.AuthCheckOrg}), s.listPendingOrgs)
 		orgs.GET("/campus", s.mustLogin, s.searchSubOrgs)
 	}
 
-	order := route.Group("/order")
+	order := api.Group("/order")
 	{
 		order.POST("/", s.mustLogin, s.hasPermission([]int{entity.AuthDispatchSelfOrder, entity.AuthDispatchOrder}), s.createOrder)
 		order.GET("/:id", s.mustLogin, s.hasPermission([]int{entity.AuthDispatchSelfOrder, entity.AuthDispatchOrder, entity.AuthListAllOrder, entity.AuthListOrgOrder}), s.getOrderByID)
 		order.PUT("/:id/signup", s.mustLogin, s.hasPermission([]int{entity.AuthListOrgOrder}), s.signupOrder)
 		order.PUT("/:id/revoke", s.mustLogin, s.hasPermission([]int{entity.AuthListOrgOrder}), s.revokeOrder)
 	}
-	orders := route.Group("/orders")
+	orders := api.Group("/orders")
 	{
 		orders.GET("/", s.mustLogin, s.hasPermission([]int{entity.AuthListAllOrder}), s.searchOrder)
 		orders.GET("/author", s.mustLogin, s.hasPermission([]int{entity.AuthDispatchOrder, entity.AuthDispatchSelfOrder}), s.searchOrderWithAuthor)
 		orders.GET("/org", s.mustLogin, s.hasPermission([]int{entity.AuthListOrgOrder}), s.searchOrderWithOrgID)
 	}
 
-	payment := route.Group("/payment")
+	payment := api.Group("/payment")
 	{
 		payment.POST("/:id/pay", s.mustLogin, s.hasPermission([]int{entity.AuthListOrgOrder}), s.payOrder)
 		payment.POST("/:id/payback", s.mustLogin, s.hasPermission([]int{entity.AuthListOrgOrder}), s.paybackOrder)
 		payment.PUT("/:id/review/accept", s.mustLogin, s.hasPermission([]int{entity.AuthCheckOrder}), s.acceptPayment)
 		payment.PUT("/:id/review/reject", s.mustLogin, s.hasPermission([]int{entity.AuthCheckOrder}), s.rejectPayment)
 	}
-	payments := route.Group("/payments")
+	payments := api.Group("/payments")
 	{
 		payments.GET("/pending", s.mustLogin, s.hasPermission([]int{entity.AuthCheckOrder}), s.searchPendingPayRecord)
 	}
-	orderSource := route.Group("/order_source")
+	orderSource := api.Group("/order_source")
 	{
 		orderSource.POST("/", s.mustLogin, s.hasPermission([]int{entity.AuthManageOrderSource}), s.createOrderSource)
 	}
-	orderSources := route.Group("/order_sources")
+	orderSources := api.Group("/order_sources")
 	{
 		orderSources.GET("/", s.mustLogin, s.listOrderSources)
 	}
 
-	statistics := route.Group("/statistics")
+	statistics := api.Group("/statistics")
 	{
 		statistics.GET("/summary", s.mustLogin, s.hasPermission([]int{entity.AuthListAllOrder}), s.summary)
 		statistics.GET("/graph", s.mustLogin, s.hasPermission([]int{entity.AuthListAllOrder}), s.graph)
