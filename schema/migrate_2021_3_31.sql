3d28729fa6715c2f7f0daadbeda4973a4d7770e4
White <white.shan@badanamu.com.cn>
2021-04-06 16:01:02 +0800

    update schema

--- schema/migrate_2021_3_31.sql
+++ schema/migrate_2021_3_31.sql
@@ -7,3 +7,14 @@ SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
 
 UPDATE auths SET mode = 1 WHERE id not in (6,13);
 UPDATE auths SET mode = 2 WHERE id in (6,13);
+INSERT INTO xg2.role_auths(role_id, auth_id) VALUES(1,14);
+INSERT INTO xg2.role_auths(role_id, auth_id) VALUES(6,14);
+
+
+ALTER TABLE `xg2`.`commission_settlement_records` 
+CHANGE COLUMN `settlement_note` `settlement_note` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_general_ci' NULL DEFAULT NULL ;
+
+
+ALTER TABLE `xg2`.`commission_settlement_records` 
+CHANGE COLUMN `note` `note` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_general_ci' NULL DEFAULT NULL ;
+INSERT INTO auths (id, name, mode) VALUES (14, "结算", 1);
