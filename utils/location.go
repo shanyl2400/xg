a6d2cc797518d3898b96770397768e0a9e0bd3a3
shanyl2400 <white.shan@badanamu.com.cn>
2021-05-28 17:55:00 +0800

    remove location

--- utils/location.go
+++ utils/location.go
@@ -1,73 +1,68 @@
 package utils
 
 import (
-	"encoding/json"
 	"errors"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"xg/conf"
 	"xg/entity"
-	"xg/log"
 )
 
-var(
-	ErrLocationNotFound = errors.New("location not found")
+var (
+	ErrLocationNotFound    = errors.New("location not found")
 	ErrLocationInfoInvalid = errors.New("location info invalid")
 )
 
 type LocationResponse struct {
-	Status string `json:"status"`
-	Info string `json:"info"`
-	Count string `json:"count"`
+	Status   string             `json:"status"`
+	Info     string             `json:"info"`
+	Count    string             `json:"count"`
 	GeoCodes []*LocationGeocode `json:"geocodes"`
 }
 type LocationGeocode struct {
 	FormattedAddress string `json:"formatted_address"`
-	Country string `json:"country"`
-	Province string `json:"province"`
-	City string `json:"city"`
-	District string `json:"district"`
-	AdCode string `json:"adcode"`
-	Location string `json:"location"`
-	Level string `json:"level"`
+	Country          string `json:"country"`
+	Province         string `json:"province"`
+	City             string `json:"city"`
+	District         string `json:"district"`
+	AdCode           string `json:"adcode"`
+	Location         string `json:"location"`
+	Level            string `json:"level"`
 }
 
-func GetAddressLocation(addr string) (*entity.Coordinate, error){
-	locationKey := conf.Get().AMapKey
-	path := fmt.Sprintf("https://restapi.amap.com/v3/geocode/geo?address=%v&output=JSON&key=%v", addr, locationKey)
-	resp, err := http.Get(path)
-	if err != nil{
-		return nil, err
-	}
-	defer resp.Body.Close()
-	resBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil{
-		return nil, err
-	}
+func GetAddressLocation(addr string) (*entity.Coordinate, error) {
+	// locationKey := conf.Get().AMapKey
+	// path := fmt.Sprintf("https://restapi.amap.com/v3/geocode/geo?address=%v&output=JSON&key=%v", addr, locationKey)
+	// resp, err := http.Get(path)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// defer resp.Body.Close()
+	// resBody, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-	locationResp := new(LocationResponse)
-	err = json.Unmarshal(resBody, locationResp)
-	if err != nil{
-		return nil, err
-	}
+	// locationResp := new(LocationResponse)
+	// err = json.Unmarshal(resBody, locationResp)
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-	if locationResp.Status != "1" {
-		return nil, errors.New(locationResp.Info)
-	}
-	count := ParseInt(locationResp.Count)
-	if count < 1 || len(locationResp.GeoCodes) < 1 {
-		return nil, ErrLocationNotFound
-	}
+	// if locationResp.Status != "1" {
+	// 	return nil, errors.New(locationResp.Info)
+	// }
+	// count := ParseInt(locationResp.Count)
+	// if count < 1 || len(locationResp.GeoCodes) < 1 {
+	// 	return nil, ErrLocationNotFound
+	// }
 
-	ret := ParseFloats(locationResp.GeoCodes[0].Location)
-	if len(ret) < 2 {
-		return nil, ErrLocationInfoInvalid
-	}
+	// ret := ParseFloats(locationResp.GeoCodes[0].Location)
+	// if len(ret) < 2 {
+	// 	return nil, ErrLocationInfoInvalid
+	// }
 
-	log.Info.Printf("Get address: %#v, resp: %#v", addr, locationResp.GeoCodes[0])
-	return &entity.Coordinate{
-		Longitude: ret[0],
-		Latitude:  ret[1],
-	}, nil
+	// log.Info.Printf("Get address: %#v, resp: %#v", addr, locationResp.GeoCodes[0])
+	// return &entity.Coordinate{
+	// 	Longitude: ret[0],
+	// 	Latitude:  ret[1],
+	// }, nil
+	return &entity.Coordinate{Longitude: 0, Latitude: 0}, nil
 }
